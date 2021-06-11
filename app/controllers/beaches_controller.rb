e03f6465f4436434118178ac79a0ebbf03fb2902
AlexanderBowers <7319abowers@gmail.com>
2021-06-11 12:24:32 -0700

    changed from beach to beaches

--- app/controllers/beaches_controller.rb
+++ app/controllers/beaches_controller.rb
@@ -1,25 +1,28 @@
 class BeachesController < ApplicationController
 
     def index
-        beaches = Beaches.all
+        beaches = []
+        Beaches.all.map do |b|
+            beaches.push(b)
+        end 
         render json: beaches
     end
 
     def show
-        beach = Beach.find_by(name: params[:name], location: params[:location])
+        beach = Beaches.find_by(name: params[:name], location: params[:location])
         render json: beach
     end
 
 
     def create
-        Beach.find_by(name: params[:name], location: params[:location]) == nil ?
-            Beach.create(name: params[:name], location: params[:location]) && response = {success: "#{params[:name]} in #{params[:location]} has been created."}
+        Beaches.find_by(name: params[:name], location: params[:location]) == nil ?
+            Beaches.create(name: params[:name], location: params[:location]) && response = {success: "#{params[:name]} in #{params[:location]} has been created."}
             : response = {error: "#{params[:name]} in #{params[:location]} already exists."}
         render json: response
     end
 
     def destroy
-        beach = Beach.find_by(id: params["id"])
+        beach = Beaches.find_by(id: params["id"])
         beach.destroy()
         render json: {message: "beach destroyed"}
     end
